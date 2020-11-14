552b809b5d56857d6a7e21eca15223695de2a188
Максим Болотов <primarch.sangvinius@hotmail.com>
2020-11-14 14:15:39 +0300

    heresy!!!

--- TV manager/Form1.Designer.cs
+++ TV manager/Form1.Designer.cs
@@ -28,23 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 450);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(332, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(655, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 52);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TV manager/Form1.cs
+++ TV manager/Form1.cs
@@ -8,14 +8,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Timers;
 
 namespace TV_manager
 {
     public partial class Form1 : Form
     {
+        // делегат для манипулирования лейбом  
+        public delegate void DelegateForTime(Label label);
+        // поле типа делегата  
+        DelegateForTime DelTime;
+        int sec = 0; string test = "";
+        // поток  
+        Thread t1, t2;
         public Form1()
         {
             InitializeComponent();
+            DelTime = new DelegateForTime(StartTime);// указываем метод делегату  
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -23,6 +33,49 @@ namespace TV_manager
             WindowState = FormWindowState.Normal;//см. текст ниже.
             FormBorderStyle = FormBorderStyle.None;
             WindowState = FormWindowState.Maximized;
+            pictureBox1.Image = Image.FromFile(@"C:\6-23.jpg");
+
+
+            /*---------------------------------------------------------------------*/
+
+            t1 = new Thread(LabelTime); // создаем поток  
+            t1.IsBackground = true; // задаем фоновый режым  
+            t1.Priority = ThreadPriority.Lowest; // указываем свмый низкий приоритет  
+            t1.Start(); // стартуем  
+            t2 = new Thread(Pict);
+            t2.IsBackground = true; // задаем фоновый режым  
+            t2.Priority = ThreadPriority.Lowest; // указываем свмый низкий приоритет  
+        }
+        void Pict()
+        {
+            while (true)
+            {
+                if (sec == 15)
+                {
+                    test = "hi"; sec = 0;
+                    pictureBox1.Image = Image.FromFile(@"C:\7864.jpg");
+                }
+            }
+        }
+        void StartTime(Label label)
+        {
+            // выводим всегда две цыфры   
+            // (00:00)  
+            string s = DateTime.Now.Hour.ToString("00");
+            s += " : ";
+            s += DateTime.Now.Minute.ToString("00");
+
+            s += " : " + DateTime.Now.Second.ToString("00");
+            sec += 1;
+            label.Text = s;
+        }
+        void LabelTime()
+        {
+            // безконечный цыкл  
+            while (true)
+            {
+                Invoke(DelTime, label1);// запускаем метод с главного потока          
+            }
         }
     }
 }
